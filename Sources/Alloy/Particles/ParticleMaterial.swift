31faccb473a8cbf67409e5afffd13d313ea2697c
alexstrnik <alex.str.nik@gmail.com>
2025-02-13 05:38:13 +0300

    fix: ParticleMaterial

--- Sources/Alloy/Particles/ParticleMaterial.swift
+++ Sources/Alloy/Particles/ParticleMaterial.swift
@@ -9,20 +9,20 @@ import Foundation
 import Metal
 
 open class AEParticleMaterial: AEUnlitMaterial {
-    override func getVertexShader() -> AEShader {
+    open override func getVertexShader() -> AEShader {
         AEShader(named: "particles_vertex_shader")
     }
 
-    override func getFragmentShader() -> AEShader {
+    open override func getFragmentShader() -> AEShader {
         AEShader(named: "particles_fragment_shader")
     }
 
-    override func makeRenderPipelineDescriptor() -> MTLRenderPipelineDescriptor {
+    open override func makeRenderPipelineDescriptor() -> MTLRenderPipelineDescriptor {
         let pipelineDescriptor = MTLRenderPipelineDescriptor()
 
         pipelineDescriptor.vertexFunction = getVertexShader().load()
         pipelineDescriptor.fragmentFunction = getFragmentShader().load()
-        pipelineDescriptor.vertexDescriptor = Vertex.descriptor
+        pipelineDescriptor.vertexDescriptor = AEVertex.descriptor
         pipelineDescriptor.colorAttachments[0].pixelFormat = AERenderer.colorPixelFormat
         pipelineDescriptor.depthAttachmentPixelFormat = AERenderer.depthStencilPixelFormat
 
